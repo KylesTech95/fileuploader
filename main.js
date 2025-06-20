a8a00ba0ec21a067d22c1eee8ec925782292b0f6
Primary User <primaryuser@Primarys-MacBook-Air.local>
2025-06-20 08:17:49 -0400

    update: add garbage tool to left bar

--- main.js
+++ main.js
@@ -6,14 +6,18 @@ const header = document.getElementById('header-mast')
 const garbage = document.getElementById('garbage-img-tool');
 const fileinfo = document.getElementById('file-info');
 const select_counter = document.querySelector('.select-counter')
-let selectedFiles = []
+let views = [...document.querySelectorAll('.view-tool')]
+let garbage_clone = garbage.cloneNode(true)
+    garbage_clone.classList.add('garbage-clone')
+    garbage_clone.setAttribute('id','garbage-img-clone')
 
-//________________________________
-// set file upload icon to middle of screen
 let midheight = window.innerHeight/2
 let isShift = false;
 let isCtl = false;
 let isMeta = false;
+let selectedFiles = []
+
+//________________________________
 
 // center file upload
 fileobj.buttons.img.style.top = ((filecontainer.clientHeight/2) + (header.clientHeight)) + "px"
@@ -31,24 +35,23 @@ fileobj.input.onchange = e => fileSystemChange(e)
 
 // garback onclick event
 garbage.onclick = e => {
-    isShift = false;
-    isCtl = false;
-    isMeta = false;
-    
-    tools.forEach(tool=> {
-        tool.classList.add('disabled-tool');
-    })
+            isShift = false;
+            isCtl = false;
+            isMeta = false;
+            
+            tools.forEach(tool=> {
+                tool.classList.add('disabled-tool');
+            })
 
-    if(!e.currentTarget.classList.contains('no-pointer')){
-        let currSelectedFiles = [...document.querySelectorAll('.file-obj-entity')].filter(x=>x.selected===true);
-
-        selectedFiles = deleteFiles(currSelectedFiles,[select_counter,e.currentTarget],selectedFiles)
-    } else {
-        console.log('garbage is disabled!')
+            if(!e.currentTarget.classList.contains('no-pointer')){
+                let currSelectedFiles = [...document.querySelectorAll('.file-obj-entity')].filter(x=>x.selected===true);
+                    selectedFiles = deleteFiles(currSelectedFiles,[select_counter,e.currentTarget,garbage_clone],selectedFiles)
+            } else {
+                console.log('garbage is disabled!')
+            }
     }
-}
 
-let views = [...document.querySelectorAll('.view-tool')]//.filter(y=>y.id!=='scroll-v');
+// handle each view
 views.forEach(view=>{
     view.onclick = e => {
         views.forEach(v=>{
@@ -67,7 +70,6 @@ views.forEach(view=>{
         switchView(document.querySelectorAll('.file-obj-entity'),e.currentTarget,document.getElementById('file-hold-container'))
 }
 })
-
 
 
 
