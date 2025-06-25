fbf94b29a50fe523b5de1fbff16200db8c159d15
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-06-25 09:26:37 -0400

    test: upload files in bins

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const path = require('path')
 const app = express();
 const cors = require('cors')
 const fileupload = require('express-fileupload');
+const ffmpeg = require('ffmpeg')
 let interval, speed = 100;
 const [input,output] = ['input','output']
 
@@ -31,12 +32,13 @@ app.route('/test').get((req,res)=>{
 //})
 
 // upload files (single/multi)
-app.route('/upload').post((req,res)=>{
+app.route('/upload').post((req,res,next)=>{
     const {image} = req.files // array
     console.log(image)
-    let folderType;
+    let folderType, len;
     try{
         if(image.length>0){
+            len = image.length
             // readfile
             image.forEach((file,index)=>{
                 folderType = file.mimetype.split`/`[0]
@@ -48,6 +50,7 @@ app.route('/upload').post((req,res)=>{
                 })
             })
         } else {
+                len = 1
                 folderType = image.mimetype.split`/`[0]
                 console.log(folderType)
                 image.mv(path.resolve(__dirname,input,folderType,image.name), err=>{
@@ -56,6 +59,7 @@ app.route('/upload').post((req,res)=>{
                     }
                 })
             }
+            res.json({data:`${len} ${len<2?'file':'files'} uploaded to the server`})
     }
     catch(err){
         throw new Error(err)
@@ -64,24 +68,31 @@ app.route('/upload').post((req,res)=>{
 
 // convert (get)
 app.route('/convert').get(async(req,res)=>{
-    const {ext} = req.query // inbin, outbin, input-files
+    const {type,ext} = req.query // inbin, outbin, input-files
     const dirFromConversion = '..', inbin = `${dirFromConversion}/${input}`, outbin = `${dirFromConversion}/${output}` // bins for input and output
     // console.log(req.query)
     // console.log(inbin)
     // console.log(outbin)
     console.log(ext)
-    const files = [...fs.readdirSync(path.resolve(__dirname,input),'utf-8')]
+    const files = [...fs.readdirSync(path.resolve(__dirname,input,type),'utf-8')];
+
+    let folderType;
     // console.log(files) 
     try{
         if(files && files.length>0){
-
             for(let i = 0; i < files.length; i++){
                 let inpFileName = files[i].split('.')[0];
-                // while loop
-                console.log('Conversion #' + (i+1))
-                convert(inbin,files[i],`${inpFileName}.${ext}`,outbin); // jpg to png
-                // convert(input,files[i],output,{height:256,width:300}); // jpg to png
-                // clear the loop
+                let currentExt = files[i].split('.')[1];
+                if(currentExt!==ext){ // if extension does not match
+                    console.log('Conversion #' + (i+1))
+                    convert(inbin,files[i],`${inpFileName}.${ext}`,outbin); // jpg to png
+                    // convert(input,files[i],output,{height:256,width:300}); // jpg to png
+                    // clear the loop// while loop
+                    console.log('Conversion #' + (i+1))
+                    convert(inbin,files[i],`${inpFileName}.${ext}`,outbin); // jpg to png
+                    // convert(input,files[i],output,{height:256,width:300}); // jpg to png
+                    // clear the loop
+                }
             }
           
         }
